5ada598b0d9799cd2555eb9c87fd8854b359c62c
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2026-05-28 11:51:53 -0400

    utlist: Add CDL_CONCAT (#280)
    Thanks to xunicatt for the patch!

--- tests/test63.c
+++ tests/test63.c
@@ -85,10 +85,45 @@ static void test_dl_concat() {
     }
 }
 
+static void test_cdl_concat_case(int a, int b) {
+    struct List els[] = {
+        {1, &dummy, &dummy},
+        {2, &dummy, &dummy},
+        {3, &dummy, &dummy},
+        {4, &dummy, &dummy},
+        {5, &dummy, &dummy},
+        {6, &dummy, &dummy},
+    };
+    struct List *list1 = NULL;
+    struct List *list2 = NULL;
+
+    for (int i = 0; i < a; ++i) {
+        CDL_APPEND(list1, &els[i]);
+    }
+    for (int i = 0; i < b; ++i) {
+        CDL_APPEND(list2, &els[i+3]);
+    }
+    CDL_CONCAT(list1, list2);
+
+    printf("### %d %d\n", a, b);
+    printf("list1 = %d, list2 = %d\n", list1 ? list1->value : -1, list2 ? list2->value : -1);
+    dump_els(list1, list2, els, 6);
+}
+
+static void test_cdl_concat() {
+    printf("## test_cdl_concat\n");
+    for (int a = 0; a <= 3; ++a) {
+        for (int b = 0; b <= 3; ++b) {
+            test_cdl_concat_case(a, b);
+        }
+    }
+}
+
 int main()
 {
     test_ll_concat();
     test_dl_concat();
+    test_cdl_concat();
 
     return 0;
 }
